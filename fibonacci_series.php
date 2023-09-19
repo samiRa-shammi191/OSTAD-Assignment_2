80689ab4eca81cc7ec347c80a28c51fd42ae2d1e
Samira Jahan <samirashammi67@gmail.com>
2023-09-20 02:43:07 +0600

    task 4  commit

---
+++ fibonacci_series.php
@@ -0,0 +1,20 @@
+<?php
+function print_fibonacci_series($n) {
+  $first = 0;
+  $second = 1;
+
+  echo "The first $n Fibonacci numbers are:\n";
+
+  for ($i = 0; $i < $n; $i++) {
+    $next = $first + $second;
+
+    echo $next . "\n";
+
+    $first = $second;
+    $second = $next;
+  }
+}
+
+
+print_fibonacci_series(15);
+?>
