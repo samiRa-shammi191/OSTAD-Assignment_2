f9ea593c3dc1e0fc6466ba92e1c3772dec94218d
Samira Jahan <samirashammi67@gmail.com>
2023-09-20 01:52:38 +0600

    while loop commit

--- looping_with_increment.php
+++ looping_with_increment.php
@@ -11,6 +11,21 @@ print_EvenNumbers_With_ForLoop(2, 20, 2);
 echo "<br>";
 
 
+function print_EvenNumbers_With_WhileLoop($start, $end, $step) {
+    $i = $start;
+    while ($i <= $end) {
+        echo $i . ' ';
+        $i += $step;
+    }
+}
+
+print_EvenNumbers_With_WhileLoop(2, 20, 2);
+echo "<br>";
+
+
+
+
+
 
 
 
