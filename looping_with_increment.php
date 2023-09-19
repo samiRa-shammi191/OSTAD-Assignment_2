1d3e9e85ab7c775b7888f5b0e40c591addb7276a
Samira Jahan <samirashammi67@gmail.com>
2023-09-20 01:57:08 +0600

    do  while loop commit

--- looping_with_increment.php
+++ looping_with_increment.php
@@ -9,6 +9,8 @@ function print_EvenNumbers_With_ForLoop($start, $end, $step) {
 
 print_EvenNumbers_With_ForLoop(2, 20, 2);
 echo "<br>";
+echo "<br>";
+
 
 
 function print_EvenNumbers_With_WhileLoop($start, $end, $step) {
@@ -21,6 +23,18 @@ function print_EvenNumbers_With_WhileLoop($start, $end, $step) {
 
 print_EvenNumbers_With_WhileLoop(2, 20, 2);
 echo "<br>";
+echo "<br>";
+
+
+function print_EvenNumbers_With_DoWhileLoop($start, $end, $step) {
+    $i = $start;
+    do {
+        echo $i . ' ';
+        $i += $step;
+    } while ($i <= $end);
+}
+
+print_EvenNumbers_With_DoWhileLoop(2, 20, 2);
 
 
 
